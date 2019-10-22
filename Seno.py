b159156b023bf147fbdd266e42de2fdc9261e7bf
Default user <ac.calderonz@uniandes.edu.co>
2019-10-22 18:46:49 +0000

    Cos

--- Seno.py
+++ Seno.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pylab as plt
 x= np.linspace(-2*np.pi, 2*np.pi,20)
-y= np.sin(x)
+y= np.cos(x)
 print (y)
+
+plt.figure()
+plt.plot(x,y)
+plt.savefig('Grafcos.png')
